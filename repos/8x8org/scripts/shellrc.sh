739f9f6c1668281ea28eb55b4763b3caf419d992
FlashTM8 <xorgbytm8@gmail.com>
2026-01-17 02:18:58 +0000

    Add portable shell shortcuts (8x/8xr)

---
+++ repos/8x8org/scripts/shellrc.sh
@@ -0,0 +1,13 @@
+# 8x8org shortcuts (portable)
+
+# Replit
+if [ -d "/home/runner/workspace" ]; then
+  alias 8x='cd /home/runner/workspace'
+  alias 8xr='cd /home/runner/workspace/repos/8x8org'
+fi
+
+# Termux
+if [ -d "$HOME/storage/shared/Workspace" ]; then
+  alias 8x='cd "$HOME/storage/shared/Workspace"'
+  alias 8xr='cd "$HOME/storage/shared/Workspace/repos/8x8org"'
+fi
